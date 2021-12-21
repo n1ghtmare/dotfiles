b0931cfeb1c8006d475b1930c663513d7858daf6
Dimitar Dimitrov <dimitrov2k@gmail.com>
2021-12-21 23:57:53 +0200

    customized the colors for lualine

--- nvim/lua/c/lualine.lua
+++ nvim/lua/c/lualine.lua
@@ -1,13 +1,51 @@
 local status_ok, lualine = pcall(require, "lualine")
 
-if not status_ok then 
+if not status_ok then
     return
 end
+
+-- TODO: Port the entire theme to lua
+local colors = {
+    black = "#000000",
+    hot_pink = "#ff0088",
+    white = "#ffffff",
+    gray_900 = "#121212",
+    gray_800 = "#323232",
+    gray_700 = "#535353",
+    gray_600 = "#737373",
+    gray_500 = "#949494",
+    gray_400 = "#b4b4b4",
+    gray_300 = "#d5d5d5",
+    gray_200 = "#f5f5f5",
+    zinc_gray_900 = "#212121",
+    zinc_gray_600 = "#7a7a7a",
+    zinc_gray_500 = "#787878",
+    zinc_gray_200 = "#b0b0b0",
+}
+
+local noirblaze_theme = {
+    normal = {
+        a = { fg = colors.gray_300, bg = colors.gray_800, gui = "bold" },
+        b = { fg = colors.gray_400, bg = colors.zinc_gray_900 },
+        c = { fg = colors.gray_400, bg = colors.gray_800 },
+    },
+
+    insert = { a = { fg = colors.gray_900, bg = colors.gray_300, gui = "bold" } },
+    visual = { a = { fg = colors.gray_900, bg = colors.hot_pink, gui = "bold" } },
+    replace = { a = { fg = colors.gray_900, bg = colors.gray_200, gui = "bold" } },
+
+    -- TODO: Figure out a what this shit below is used for
+    inactive = {
+        a = { fg = colors.gray_200, bg = colors.gray_900 },
+        b = { fg = colors.gray_200, bg = colors.gray_900 },
+        c = { fg = colors.gray_200, bg = colors.gray_900 },
+    },
+}
 
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'auto',
+    theme = noirblaze_theme,
 
     filetype = {
         colored = false
