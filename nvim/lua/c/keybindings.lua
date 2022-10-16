59f8c417a033c85c4946ccc8384c49daffd85570
Dimitar Dimitrov <dimitrov2k@gmail.com>
2022-10-17 00:53:10 +0300

    neovim: new keybindings and refactoring

--- nvim/lua/c/keybindings.lua
+++ nvim/lua/c/keybindings.lua
@@ -1,59 +1,73 @@
 local opts = { noremap = true, silent = true }
 local m = {}
 
+local set_keymap = vim.api.nvim_set_keymap
+local buf_set_keymap = vim.api.nvim_buf_set_keymap
+
 vim.g.mapleader = " " -- map leader to space
 
 -- Basic
 -- Clear search results by pressing _
-vim.api.nvim_set_keymap("n", "_", ":nohl<CR>", opts);
+set_keymap("n", "_", ":nohl<CR>", opts);
+
+-- Paste to a black hole register so that you can paste multiple times over a visual selection
+-- set_keymap("x", "<leader>p", "\"_dP", opts)
+
+-- Move lines up and down
+-- Move line up and down and set the formatting correctly
+set_keymap("n", "<A-j>", ":move .+1<CR>==", opts)
+set_keymap("n", "<A-k>", ":move .-2<CR>==", opts)
+-- Move visual selection up and down (including a visual block)
+set_keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+set_keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- Sane split shortcuts
-vim.api.nvim_set_keymap("n", "<C-j>", "<C-W><C-J>", opts)
-vim.api.nvim_set_keymap("n", "<C-k>", "<C-W><C-K>", opts)
-vim.api.nvim_set_keymap("n", "<C-l>", "<C-W><C-L>", opts)
-vim.api.nvim_set_keymap("n", "<C-h>", "<C-W><C-H>", opts)
+set_keymap("n", "<C-j>", "<C-W><C-J>", opts)
+set_keymap("n", "<C-k>", "<C-W><C-K>", opts)
+set_keymap("n", "<C-l>", "<C-W><C-L>", opts)
+set_keymap("n", "<C-h>", "<C-W><C-H>", opts)
 
 -- NvimTree
-vim.api.nvim_set_keymap("n", "<leader>`", ":NvimTreeToggle<CR>", opts)                      -- NvimTree toggle <leader>`
--- vim.api.nvim_set_keymap("n", "<leader>`", "<cmd>Telescope file_browser<CR>", opts)                      -- NvimTree toggle <leader>`
+set_keymap("n", "<leader>`", ":NvimTreeToggle<CR>", opts) -- NvimTree toggle <leader>`
+-- set_keymap("n", "<leader>`", "<cmd>Telescope file_browser<CR>", opts)                      -- NvimTree toggle <leader>`
 
 -- Telescope
-vim.api.nvim_set_keymap("n", "<C-p>", "<cmd>Telescope find_files<CR>", opts)                -- Ctrl-p - find files
-vim.api.nvim_set_keymap("n", "<C-e>", "<cmd>Telescope live_grep<CR>", opts)                 -- Ctrl-e - search with grep (contents of files)
-vim.api.nvim_set_keymap("n", "<C-b>", "<cmd>Telescope buffers<CR>", opts)                   -- Ctrl-b - search current buffers
-vim.api.nvim_set_keymap("n", "<leader>bf", "<cmd>Telescope current_buffer_fuzzy_find<CR>", opts) -- Ctrl-/ - search the current buffer with fuzzy find
-vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", opts)            -- space-fh - search help tags in nvim as well as for all installed plugins
-vim.api.nvim_set_keymap("n", "<leader>ft", "<cmd>Telescope git_files<CR>", opts)            -- space-ft - search for git files only
-vim.api.nvim_set_keymap("n", "<leader>ts", "<cmd>Telescope tagstack<CR>", opts)            -- space-ft - search for git files only
-vim.api.nvim_set_keymap("n", "<leader>mk", "<cmd>Telescope marks<CR>", opts)            -- space-ft - search for git files only
+set_keymap("n", "<C-p>", "<cmd>Telescope find_files<CR>", opts) -- Ctrl-p - find files
+set_keymap("n", "<C-e>", "<cmd>Telescope live_grep<CR>", opts) -- Ctrl-e - search with grep (contents of files)
+set_keymap("n", "<C-b>", "<cmd>Telescope buffers<CR>", opts) -- Ctrl-b - search current buffers
+set_keymap("n", "<leader>bf", "<cmd>Telescope current_buffer_fuzzy_find<CR>", opts) -- Ctrl-/ - search the current buffer with fuzzy find
+set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", opts) -- space-fh - search help tags in nvim as well as for all installed plugins
+set_keymap("n", "<leader>ft", "<cmd>Telescope git_files<CR>", opts) -- space-ft - search for git files only
+set_keymap("n", "<leader>ts", "<cmd>Telescope tagstack<CR>", opts) -- space-ft - search for git files only
+set_keymap("n", "<leader>mk", "<cmd>Telescope marks<CR>", opts) -- space-ft - search for git files only
 
 -- undotree
-vim.api.nvim_set_keymap("n", "<leader>ut", "<cmd>UndotreeToggle<CR>", opts)
+set_keymap("n", "<leader>ut", "<cmd>UndotreeToggle<CR>", opts)
 
 -- Todo Comments
-vim.api.nvim_set_keymap("n", "<leader>td", "<cmd>TodoQuickFix<CR>", opts)
+set_keymap("n", "<leader>td", "<cmd>TodoQuickFix<CR>", opts)
 
 -- LSP
 function m.lsp_keybindings_for_buffer(bufnr)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>k", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gi", "<cmd>Telescope lsp_implementations<CR>", opts)
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>l", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gr", "<cmd>Telescope lsp_references<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>st", "<cmd>Telescope git_status<CR>", opts)
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ac", "<cmd>Telescope lsp_code_actions<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ac", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>Telescope diagnostics<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ff", "<cmd>lua vim.lsp.buf.format { async = true }<CR>", opts)
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+    buf_set_keymap(bufnr, "n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    buf_set_keymap(bufnr, "n", "<leader>gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+    -- buf_set_keymap(bufnr, "n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    buf_set_keymap(bufnr, "n", "<leader>k", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    buf_set_keymap(bufnr, "n", "<leader>gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+    -- buf_set_keymap(bufnr, "n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    buf_set_keymap(bufnr, "n", "<leader>l", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    buf_set_keymap(bufnr, "n", "<leader>gr", "<cmd>Telescope lsp_references<CR>", opts)
+    buf_set_keymap(bufnr, "n", "<leader>st", "<cmd>Telescope git_status<CR>", opts)
+    -- buf_set_keymap(bufnr, "n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    -- buf_set_keymap(bufnr, "n", "<leader>ac", "<cmd>Telescope lsp_code_actions<CR>", opts)
+    buf_set_keymap(bufnr, "n", "<leader>ac", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+    buf_set_keymap(bufnr, "n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+    buf_set_keymap(bufnr, "n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+    buf_set_keymap(bufnr, "n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+    buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>Telescope diagnostics<CR>", opts)
+    buf_set_keymap(bufnr, "n", "<leader>ff", "<cmd>lua vim.lsp.buf.format { async = true }<CR>", opts)
+    -- buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
     --vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
 
--- nvim/lua/c/startify.lua
+++ nvim/lua/c/startify.lua
@@ -1,3 +1,9 @@
+local status_ok, nvim_web_devicons = pcall(require, "nvim-web-devicons")
+
+if not status_ok then
+    return
+end
+
 vim.g.startify_session_persistence = 1
 
 vim.g.startify_custom_header = {
@@ -15,7 +21,7 @@ vim.g.startify_bookmarks = {'~/Dev-Config/dotfiles'}
 function _G.webDevIcons(path)
   local filename = vim.fn.fnamemodify(path, ':t')
   local extension = vim.fn.fnamemodify(path, ':e')
-  return require'nvim-web-devicons'.get_icon(filename, extension, { default = true })
+  return nvim_web_devicons.get_icon(filename, extension, { default = true })
 end
 
 vim.cmd([[
