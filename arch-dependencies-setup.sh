4712169dc49b69c42a57fae154e6e86aa9838f97
Dimitar Dimitrov <dimitrov2k@gmail.com>
2023-02-09 20:52:07 +0200

    rofi emoji selector with its own sxhkd hotkey, update arch dependencies scripts

--- arch-dependencies-setup.sh
+++ arch-dependencies-setup.sh
@@ -1,2 +1,2 @@
 # Make sure you install ttf-iosevka, cava from yay
-pacman -S bspwm sxhkd starship picom polybar zathura tmux zsh dunst noto-fonts noto-fonts-emoji ttf-liberation ttf-caladea ttf-carlito ttf-opensans otf-overpass ttf-ubuntu-font-family flameshot xclip breeze pulseaudio pavucontrol bpytop ttf-jetbrains-mono thunar lxappearance zip unzip p7zip exa npm nodejs ttf-dejavu ttf-bitstream-vera
+pacman -S bspwm sxhkd starship picom polybar zathura tmux zsh dunst noto-fonts noto-fonts-emoji ttf-liberation ttf-caladea ttf-carlito ttf-opensans otf-overpass ttf-ubuntu-font-family flameshot xclip breeze pulseaudio pavucontrol bpytop ttf-jetbrains-mono thunar lxappearance zip unzip p7zip exa npm nodejs ttf-dejavu ttf-bitstream-vera rofimoji xdotool
