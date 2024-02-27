5a21a781bb7117c9b21e9ecd01dc2d8e725407b1
Dimitar Dimitrov <dimitrov2k@gmail.com>
2024-02-27 18:23:58 +0200

    Fix neovim copilot plugin bug

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,6 +1,6 @@
 return {
     "github/copilot.vim",
-    lazy = true,
+    lazy = false,
     config = function()
         vim.g.copilot_assume_mapped = true
         vim.g.copilot_no_tab_map = true
