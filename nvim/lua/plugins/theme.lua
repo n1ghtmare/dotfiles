52df1fc9cb5081d248f9b3d2867c231d7e25c1c4
Dimitar Dimitrov <dimitrov2k@gmail.com>
2024-05-10 00:17:32 +0300

    Fix noirbuddy branch dependency

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,24 +1,23 @@
 return {
     "jesseleite/nvim-noirbuddy",
     dependencies = {
-        { "tjdevries/colorbuddy.nvim", branch = "dev" }
+        { "tjdevries/colorbuddy.nvim" },
     },
     lazy = false,
     priority = 1000,
     opts = {
         colors = {
-            diagnostic_info = '#d5d5d5',
-            diagnostic_hint = '#f5f5f5',
+            diagnostic_info = "#d5d5d5",
+            diagnostic_hint = "#f5f5f5",
             diff_add = "#00ff77",
             diff_change = "#d5d5d5",
             diff_delete = "#ff0038",
         },
-        -- hello
         styles = {
             italic = false,
             undercurl = false,
             bold = true,
             underline = true,
-        }
+        },
     },
 }
