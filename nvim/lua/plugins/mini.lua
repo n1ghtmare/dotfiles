0ffcea74250ae62edea5a0b745e7f7b7fb0f3a44
Dimitar Dimitrov <dimitrov2k@gmail.com>
2024-10-08 18:53:08 +0300

    Add colorizer plugin to neovim, adjust startup screen for mini starter

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -6,7 +6,23 @@ return {
         -- require("mini.pairs").setup()
         require("mini.surround").setup()
         require("mini.comment").setup()
-        require("mini.starter").setup()
+        require("mini.starter").setup({
+            header = function()
+                local val = [[
+    ███▄    █ ▓█████  ▒█████   ██▒   █▓ ██▓ ███▄ ▄███▓    ▄▄▄▄   ▄▄▄█████▓ █     █░
+     ██ ▀█   █ ▓█   ▀ ▒██▒  ██▒▓██░   █▒▓██▒▓██▒▀█▀ ██▒   ▓█████▄ ▓  ██▒ ▓▒▓█░ █ ░█░
+    ▓██  ▀█ ██▒▒███   ▒██░  ██▒ ▓██  █▒░▒██▒▓██    ▓██░   ▒██▒ ▄██▒ ▓██░ ▒░▒█░ █ ░█
+    ▓██▒  ▐▌██▒▒▓█  ▄ ▒██   ██░  ▒██ █░░░██░▒██    ▒██    ▒██░█▀  ░ ▓██▓ ░ ░█░ █ ░█
+    ▒██░   ▓██░░▒████▒░ ████▓▒░   ▒▀█░  ░██░▒██▒   ░██▒   ░▓█  ▀█▓  ▒██▒ ░ ░░██▒██▓
+    ░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒░▒░▒░    ░ ▐░  ░▓  ░ ▒░   ░  ░   ░▒▓███▀▒  ▒ ░░   ░ ▓░▒ ▒
+    ░ ░░   ░ ▒░ ░ ░  ░  ░ ▒ ▒░    ░ ░░   ▒ ░░  ░      ░   ▒░▒   ░     ░      ▒ ░ ░
+       ░   ░ ░    ░   ░ ░ ░ ▒       ░░   ▒ ░░      ░       ░    ░   ░        ░   ░
+             ░    ░  ░    ░ ░        ░   ░         ░       ░                   ░
+                                    ░                           ░
+]]
+                return val
+            end,
+        })
         require("mini.trailspace").setup()
         require("mini.ai").setup()
         -- require("mini.indentscope").setup()
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,4 +1,5 @@
 return {
+    -- dir = "~/Dev/nvim-noirbuddy/",
     "jesseleite/nvim-noirbuddy",
     dependencies = {
         { "tjdevries/colorbuddy.nvim" },
@@ -6,13 +7,15 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
-        colors = {
-            diagnostic_info = "#d5d5d5",
-            diagnostic_hint = "#f5f5f5",
-            diff_add = "#00ff77",
-            diff_change = "#d5d5d5",
-            diff_delete = "#ff0038",
-        },
+        preset = "oxide",
+        -- colors = {
+        --     -- primary = "#00d992",
+        --     -- diagnostic_info = "#d5d5d5",
+        --     -- diagnostic_hint = "#f5f5f5",
+        --     -- diff_add = "#00ff77",
+        --     -- diff_change = "#d5d5d5",
+        --     -- diff_delete = "#ff0038",
+        -- },
         styles = {
             italic = false,
             undercurl = false,
