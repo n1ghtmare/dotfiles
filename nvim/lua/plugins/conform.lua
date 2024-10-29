52fb587bf4a6616b45691a94f2b6dd709f0f3a26
Dimitar Dimitrov <dimitrov2k@gmail.com>
2024-10-29 20:12:17 +0200

    Update `conform.nvim` to work with `vue`

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -10,6 +10,7 @@ return {
             javascriptreact = { "prettierd", "prettier", stop_after_first = true },
             typescript = { "prettierd", "prettier", stop_after_first = true },
             typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+            vue = { "prettierd", "prettier", stop_after_first = true },
             json = { "prettierd", "prettier", stop_after_first = true },
             css = { "prettierd", "prettier", stop_after_first = true },
             html = { "prettierd", "prettier", stop_after_first = true },
