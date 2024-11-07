466f83f0ef497daf616532d3f478b49e500d81dc
Dimitar Dimitrov <dimitrov2k@gmail.com>
2024-11-07 13:49:32 +0200

    Fix `volar` (vue language server) to work with TypeScript in Neovim

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -169,7 +169,24 @@ lspconfig.bashls.setup({
 })
 
 -- Typescript
+local mason_registry = require("mason-registry")
+local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+    .. "/node_modules/@vue/language-server"
 lspconfig.ts_ls.setup({
+    init_options = {
+        plugins = {
+            {
+                -- Name of the TypeScript plugin for Vue
+                name = "@vue/typescript-plugin",
+
+                -- Location of the Vue language server module (path defined in step 1)
+                location = vue_language_server_path,
+
+                -- Specify the languages the plugin applies to (in this case, Vue files)
+                languages = { "vue" },
+            },
+        },
+    },
     capabilities = capabilities,
     on_attach = function(client, bufnr)
         client.server_capabilities.documentFormattingProvider = false
@@ -191,4 +208,12 @@ lspconfig.ts_ls.setup({
         -- this way we keep keybindings in one place
         require("keybindings").lsp_keybindings_for_buffer(bufnr)
     end,
+    -- Specify the file types that will trigger the TypeScript language server
+    filetypes = {
+        "typescript", -- TypeScript files (.ts)
+        "javascript", -- JavaScript files (.js)
+        "javascriptreact", -- React files with JavaScript (.jsx)
+        "typescriptreact", -- React files with TypeScript (.tsx)
+        "vue", -- Vue.js single-file components (.vue)
+    },
 })
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,8 +7,8 @@ return {
     },
     build = ":TSUpdate",
     config = function()
-        require("treesitter-context").setup { enable = true }
-        require("nvim-treesitter.configs").setup {
+        require("treesitter-context").setup({ enable = true })
+        require("nvim-treesitter.configs").setup({
             -- Languages to be installed with TS -> if more are needed, this is where you install them
             -- For full list of languages: https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
             -- TODO: Clean this list a bit
@@ -48,7 +48,7 @@ return {
                 "luadoc",
                 "make",
                 "markdown",
-                "markdown_inline",
+                -- "markdown_inline",
                 -- "nix",
                 "ocaml",
                 "perl",
@@ -87,8 +87,8 @@ return {
                     init_selection = "<c-space>",
                     node_incremental = "<c-space>",
                     scope_incremental = "<c-s>",
-                    node_decremental = "<c-backspace>"
-                }
+                    node_decremental = "<c-backspace>",
+                },
             },
             textobjects = {
                 select = {
@@ -97,44 +97,44 @@ return {
                     lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
                     keymaps = {
                         -- You can use the capture groups defined in textobjects.scm
-                        ['aa'] = '@parameter.outer',
-                        ['ia'] = '@parameter.inner',
-                        ['af'] = '@function.outer',
-                        ['if'] = '@function.inner',
-                        ['ac'] = '@class.outer',
-                        ['ic'] = '@class.inner',
+                        ["aa"] = "@parameter.outer",
+                        ["ia"] = "@parameter.inner",
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
                     },
                 },
                 move = {
                     enable = true,
                     set_jumps = true, -- whether to set jumps in the jumplist
                     goto_next_start = {
-                        [']m'] = '@function.outer',
-                        [']]'] = '@class.outer',
+                        ["]m"] = "@function.outer",
+                        ["]]"] = "@class.outer",
                     },
                     goto_next_end = {
-                        [']M'] = '@function.outer',
-                        [']['] = '@class.outer',
+                        ["]M"] = "@function.outer",
+                        ["]["] = "@class.outer",
                     },
                     goto_previous_start = {
-                        ['[m'] = '@function.outer',
-                        ['[['] = '@class.outer',
+                        ["[m"] = "@function.outer",
+                        ["[["] = "@class.outer",
                     },
                     goto_previous_end = {
-                        ['[M'] = '@function.outer',
-                        ['[]'] = '@class.outer',
+                        ["[M"] = "@function.outer",
+                        ["[]"] = "@class.outer",
                     },
                 },
                 swap = {
                     enable = true,
                     swap_next = {
-                        ['<leader>a'] = '@parameter.inner',
+                        ["<leader>a"] = "@parameter.inner",
                     },
                     swap_previous = {
-                        ['<leader>A'] = '@parameter.inner',
+                        ["<leader>A"] = "@parameter.inner",
                     },
                 },
             },
-        }
-    end
+        })
+    end,
 }
