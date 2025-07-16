361e0fcb41c0c85164f92e32250ab209ec2330aa
Dimitar Dimitrov <dimitrov2k@gmail.com>
2025-07-16 08:43:41 +0300

    Add new start screen for nvim, update nvim dependencies

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -6,31 +6,8 @@ return {
         -- require("mini.pairs").setup()
         require("mini.surround").setup()
         require("mini.comment").setup()
-        require("mini.starter").setup({
-            header = function()
-                local Group = require("colorbuddy").Group
-                local colors = require("colorbuddy").colors
-
-                Group.new("MiniStarterHeader", colors.primary)
-                Group.new("MiniStarterFooter", colors.secondary)
-
-                local val = [[
-    ███▄    █ ▓█████  ▒█████   ██▒   █▓ ██▓ ███▄ ▄███▓    ▄▄▄▄   ▄▄▄█████▓ █     █░
-     ██ ▀█   █ ▓█   ▀ ▒██▒  ██▒▓██░   █▒▓██▒▓██▒▀█▀ ██▒   ▓█████▄ ▓  ██▒ ▓▒▓█░ █ ░█░
-    ▓██  ▀█ ██▒▒███   ▒██░  ██▒ ▓██  █▒░▒██▒▓██    ▓██░   ▒██▒ ▄██▒ ▓██░ ▒░▒█░ █ ░█
-    ▓██▒  ▐▌██▒▒▓█  ▄ ▒██   ██░  ▒██ █░░░██░▒██    ▒██    ▒██░█▀  ░ ▓██▓ ░ ░█░ █ ░█
-    ▒██░   ▓██░░▒████▒░ ████▓▒░   ▒▀█░  ░██░▒██▒   ░██▒   ░▓█  ▀█▓  ▒██▒ ░ ░░██▒██▓
-    ░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒░▒░▒░    ░ ▐░  ░▓  ░ ▒░   ░  ░   ░▒▓███▀▒  ▒ ░░   ░ ▓░▒ ▒
-    ░ ░░   ░ ▒░ ░ ░  ░  ░ ▒ ▒░    ░ ░░   ▒ ░░  ░      ░   ▒░▒   ░     ░      ▒ ░ ░
-       ░   ░ ░    ░   ░ ░ ░ ▒       ░░   ▒ ░░      ░       ░    ░   ░        ░   ░
-             ░    ░  ░    ░ ░        ░   ░         ░       ░                   ░
-                                    ░                           ░
-]]
-                return val
-            end,
-        })
         require("mini.trailspace").setup()
         require("mini.ai").setup()
-        -- require("mini.indentscope").setup()
+        require("mini.indentscope").setup()
     end,
 }
